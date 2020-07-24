ce235c420900807983fa58860902972744fd58b3
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-24 11:25:55 +0200

    Add messages

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,11 @@
 #!/usr/bin/env ruby
-puts 'Hello world!'
+class StockPicker
+    def initialize
+        
+    end
+
+    def start
+        puts 'Hello there, welcome to the STOCKPICKER software, this software helps you identify which day is best to sell
+        and which day is best to buy'
+    end
+end

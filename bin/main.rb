69e1d8ac949fa007154bf1cfadbdaccd56951088
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-24 12:15:17 +0200

    Fix rubocop error

--- bin/main.rb
+++ bin/main.rb
@@ -1,42 +1,41 @@
 #!/usr/bin/env ruby
 class StockPicker
-    def initialize
-        @stock_prices = []
-    end
+  def initialize
+    @stock_prices = []
+  end
 
-    def start
-        puts 'Hello there, welcome to the STOCKPICKER software, this software helps you identify which day is best to sell
+  def start
+    puts 'Hello there, welcome to the STOCKPICKER software, this software helps you identify which day is best to sell
         and which day is best to buy!'
-        return unless continue?
+    return unless continue?
 
-        interested = true
-        while interested do
-            user_enters_prices
-            show_results
-            interested = false unless continue?
-        end
+    interested = true
+    while interested
+      user_enters_prices
+      show_results
+      interested = false unless continue?
     end
+  end
 
-    def user_enters_prices
-        puts "\nEnter the array which repesents your prices:"
-        @stock_prices = gets.chomp
-    end
+  def user_enters_prices
+    puts "\nEnter the array which repesents your prices:"
+    @stock_prices = gets.chomp
+  end
 
-    def analyze_stock_prices
-        
-    end
+  def analyze_stock_prices; end
 
-    def show_results
-        puts @stock_prices
-    end
+  def show_results
+    puts @stock_prices
+  end
 
-    def continue?
-        puts 'Do you want to continue? If you want to continue enter y otherwise enter any other key to terminate the STOCKPICKER!'
-        continue_value = gets.chomp.downcase
-        return true if continue_value == 'y'
+  def continue?
+    puts 'Do you want to continue? If you want to continue enter y otherwise enter any other key
+    to terminate the STOCKPICKER!'
+    continue_value = gets.chomp.downcase
+    return true if continue_value == 'y'
 
-        false
-    end
+    false
+  end
 end
 
 stock = StockPicker.new

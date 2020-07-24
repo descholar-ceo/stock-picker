4e69c44c3a4d6eec04281527c19ede1ed0e30ae0
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-24 14:49:09 +0200

    Fix rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -23,37 +23,32 @@ class StockPicker
   end
 
   def analyze_stock_prices
-    value = @stock_prices.max_by {|x| x.length}
-    return value
-    
-=begin
-
-    get the array
-    create a hash result to compare
-    create days array=[]
-
-    # analyze the array
-    on each element do
-        i = 0
-        while i<array.length-index of current element-1
-            if the difference is positive
-                    add current element to the days array
-                    add the next element to the days array
-                    add the differnce result as value and days array as key on the
-            else
-                next
-            end if
-            i++
-        end while
-    end each
-
-    # analyze the hash
-    return the pair which has the max value
-=end
+    #
+    #     get the array
+    #     create a hash result to compare
+    #     create days array=[]
+    #
+    #     # analyze the array
+    #     on each element do
+    #         i = 0
+    #         while i<array.length-index of current element-1
+    #             if the difference is positive
+    #                     add current element to the days array
+    #                     add the next element to the days array
+    #                     add the differnce result as value and days array as key on the
+    #             else
+    #                 next
+    #             end if
+    #             i++
+    #         end while
+    #     end each
+    #
+    #     # analyze the hash
+    #     return the pair which has the max value
   end
 
   def show_results
-    puts analyze_stock_prices
+    puts @stock_prices
   end
 
   def continue?
